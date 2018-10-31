0c82927ce1a31ad1e73f0b99ed2fd9f703536464
Austin  Burke <aburk3@gmail.com>
2018-10-31 22:35:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,25 +6,43 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    users = []
+    profiles = []
 
-    doc = Nokogiri::HTML(open(index_url))
+    index = Nokogiri::HTML(open(index_url))
 
-    doc.css("div.roster-cards-container").each do |card|
-      card.css("div.student-card a").each do |profile|
-        profile_link = profile.attribute("href").value
-        profile_name = profile.css(".student-name").text
-        profile_location = profile.css(".student-location").text
+    index.css("div.roster-cards-container").each do |profile|
+      profile.css("div.student-card a").each do |student|
+        profile_link = student.attribute("href").value
+        profile_location = student.css(".student-location").text
+        profile_name = student.css(".student-name").text
 
-        users << {name: profile_name, location: profile_location, profile_url: profile_link}
-
+        profiles << {name: profile_name, location: profile_location, profile_url: profile_link}
       end
-      binding.pry
+    end
+    profiles
+  end
+
+
+  def self.scrape_profile_page(profile_url)
+    student = {}
+
+    profile_page = Nokogiri::HTML(open(profile_url))
+
+    profile_page.css("div.social-icon-container a").each do |social_sites|
+      social_sites.attributes("href").each do |social|
+        # binding.pry
+        social
+      end
     end
   end
 
-  def self.scrape_profile_page(profile_url)
+  # => {:twitter=>"http://twitter.com/flatironschool",
+      #   :linkedin=>"https://www.linkedin.com/in/flatironschool",
+      #   :github=>"https://github.com/learn-co,
+      #   :blog=>"http://flatironschool.com",
+      #   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+      #   :bio=> "I'm a school"
+      #  }
 
-  end
 
 end
