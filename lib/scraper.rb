77fd1b3169f508bb0ac6568debfc81ab21752846
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-18 11:45:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read(index_url)
+    index_page = Nokogiri::HTML(html)
+    students = []
+    index_page.css("").each do |student|
+      this_student = {}
+      this_student[:name]
+      this_student[:location]
+      this_student[:profile_url]
+      @@students << this_student
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
