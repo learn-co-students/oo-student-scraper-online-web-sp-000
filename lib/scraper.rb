d8a5a205fef73b58bb4c6e7c20c4ab42b46e531b
Alison Reilly <reillyamr@gmail.com>
2019-01-15 05:55:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,7 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
+  attr_accessor 
 
   def self.scrape_index_page(index_url)
     
@@ -25,10 +26,59 @@ class Scraper
         
         
       end 
+      
+      
     return students
+    
+    
   end
 
   def self.scrape_profile_page(profile_url)
+    
+    #this retrieves students array from other method 
+    students_overall = self.scrape_index_page(profile_url)
+    #binding.pry
+    
+    #this isolates student urls into new array
+    i = 0 
+    just_url = []
+   while i < students_overall.length 
+      url = students_overall[i][:profile_url] 
+      i += 1 
+      just_url << url
+   end 
+   
+   #creates array each needed individual url for each student 
+   pages_prefix = 'fixtures/student-site/students/'
+    individual_urls = [] 
+    just_url.each do |modify| 
+        indiv_student = pages_prefix + "#{modify}" 
+        
+        individual_urls << indiv_student
+        
+      end 
+      
+      
+      #needs to look for linkedin, github, blog, quote, bio
+      j = 0 
+      
+      while j < individual_urls.length 
+       
+      profile_url = File.read(individual_urls[j])
+      student_attributes = Nokogiri::HTML(profile_url)
+        
+
+  
+ 
+    # profile_url = File.read('fixtures/student-site/students/')
+    # individual_page = Nokogiri::HTML(profile_url)
+    
+    # student_details = {}
+    
+    
+    
+    
+    
     
     
   end
