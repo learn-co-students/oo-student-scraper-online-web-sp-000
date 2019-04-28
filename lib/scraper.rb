949d7ee82bdacef61155bbcc2ef6b82eec1f90e6
Alexander Baker <alex.baker685@gmail.com>
2019-04-28 23:49:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    {:name => "Ryan Johnson", :location => "", :profile_url => },
+    {:name => , :location => , :profile_url => },
+    {:name => , :location => , :profile_url => },
+    {:name => , :location => , :profile_url => },
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
