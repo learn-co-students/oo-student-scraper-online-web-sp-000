0d632fb112a5a30c197290fcf2d45b8d9a0c8590
Yingqi Chen <chanwk18@hotmail.com>
2019-05-01 20:33:53 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,10 +7,10 @@ class Scraper
   def self.scrape_index_page(index_url)
      doc=Nokogiri::HTML(open(index_url))
      array=[]
-     binding.pry
-     doc.css(".student-card").first.each do |a|
+
+     doc.css(".student-card").each do |a|
          student={}
-         student.fetch(:name,"a.css("h4").text")
+         student[:name]=a.css("h4").text
          student[:location]=a.css("p").text
          student[:profile_url]=a.css("a")[0]['href']
          array<<student
@@ -19,9 +19,25 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    doc=Nokogiri::HTML(open(profile_url))
+    student_hash={}
+    student_hash[:profile_quote]=doc.css(".vitals-text-container .profile-quote").text
+    student_hash[:bio]=doc.css(".description-holder p").text
 
+    social_medias=doc.css(".social-icon-container").css("a")
+    social_medias.each do |s|
+      if s["href"].include?("twitter")
+        student_hash[:twitter]=s["href"]
+      elsif s["href"].include?("facebook")
+          student_hash[:facebook]=s["href"]
+        elsif  s["href"].include?("linkedin")
+            student_hash[:linkedin]=s["href"]
+          elsif  s["href"].include?("github")
+              student_hash[:github]=s["href"]
+            else student_hash[:blog]=s["href"]
+            end
+          end
+       student_hash
   end
 
 end
-
-Scraper.scrape_index_page("fixtures/student-site/index.html")
