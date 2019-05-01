0b25f31ea47d68112e7b41d3d53c886da8e9e24d
alex6851 <alex.baker685@gmail.com>
2019-05-01 13:17:18 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,20 @@
 require 'open-uri'
 require 'pry'
 
+
+
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index = open(index_url)
+    doc = Nokogiri::HTML((index))
     binding.pry
-    # doc = Nokogiri::HTML((index_url))
-    # binding.pry
+    new_array = []
+    new_array << doc.css('body','div#main-wrapper roster div#roster-cards-container')
+    binding.pry
     # open(index_url, :name => //div/a, :location => "location", :profile_url=>"students/name"){|f|
     # }
+
   end
 
   def self.scrape_profile_page(profile_url)
