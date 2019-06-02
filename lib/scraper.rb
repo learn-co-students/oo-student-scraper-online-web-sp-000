744182d1ce3839c564706be83a6ffa4a7069f4f2
Jared Grove <jaredkgrove@gmail.com>
2019-06-02 23:42:03 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read('./fixtures/student-site/index.html')
+    index_page = Nokogiri::HTML(html)
+
+    students = {}
+    #hello
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
