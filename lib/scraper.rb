340c64b5439a3d05eb546d071749c862414bef4b
Jared Grove <jaredkgrove@gmail.com>
2019-06-03 00:33:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,11 +14,8 @@ class Scraper
       location = student.css("p.student-location").text
       profile_url = student.css("a").attribute("href").value
       students << {:name => name.to_sym, :location => location.to_sym, :profile_url => profile_url.to_sym}
-    #:name => project.css("p.bbcard_blurb").text,
-    #:location => project.css("ul.project-meta span.location-name").text,
-    #:profile_url => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  #}
-end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
