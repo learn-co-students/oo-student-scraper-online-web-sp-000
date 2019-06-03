92c171d38a125885fe870db9332b95ec167c41b1
Jared Grove <jaredkgrove@gmail.com>
2019-06-03 00:33:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,13 +7,13 @@ class Scraper
     html = File.read('./fixtures/student-site/index.html')
     students_page = Nokogiri::HTML(html)
 
-    students = {}
-puts "hello"
-binding.pry
+    students = []
+
     students_page.css("div.student-card").each do |student|
-      puts student.css("h4.student-name").text
-  #title = project.css("h2.bbcard_name strong a").text
-  #student[title.to_sym] = {
+      name = student.css("h4.student-name").text
+      location = student.css("p.student-location").text
+      profile_url = student.css("a").attribute("href").value
+      students << {:name => name.to_sym, :location => location.to_sym, :profile_url => profile_url.to_sym}
     #:name => project.css("p.bbcard_blurb").text,
     #:location => project.css("ul.project-meta span.location-name").text,
     #:profile_url => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
