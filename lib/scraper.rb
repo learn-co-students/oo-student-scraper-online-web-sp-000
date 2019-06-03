a66a67ea1ebb7920f97becb3e64ee27ccea4719b
Jared Grove <jaredkgrove@gmail.com>
2019-06-03 01:15:36 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,11 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = File.read('./fixtures/student-site/index.html')
-    students_page = Nokogiri::HTML(html)
+    index_page = Nokogiri::HTML(html)
 
     students = []
 
-    students_page.css("div.student-card").each do |student|
+    index_page.css("div.student-card").each do |student|
       name = student.css("h4.student-name").text
       location = student.css("p.student-location").text
       profile_url = student.css("a").attribute("href").value
@@ -19,7 +19,22 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    html = File.read(profile_url)
+    profile_page = Nokogiri::HTML(html)
+    social_links = profile_page.css("div.social-icon-container").css("a").select {|social| social.attribute("href").value}
+    binding.pry
+
+
 
   end
+
+#  {{:twitter=>"https://twitter.com/jmburges",
+#                            :linkedin=>"https://www.linkedin.com/in/jmburges",
+#                            :github=>"https://github.com/jmburges",
+#                            :blog=>"http://joemburgess.com/",
+#                            :profile_quote=>"\"Reduce to a previously solved problem\"",
+#                            :bio=>
+#  "I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}}
+
 
 end
