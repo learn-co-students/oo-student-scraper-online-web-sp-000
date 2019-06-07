d013fc4546bf4029ec5f24cc223a24ce324cac7e
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-07 15:10:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,18 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = Nokogiri::HTML(open('./fixtures/student-site/index.html'))
     
+    html.css(".student-card").each do |student| 
+      binding.pry 
+      student.css("#id")
+    end 
+
+   
   end
 
   def self.scrape_profile_page(profile_url)
