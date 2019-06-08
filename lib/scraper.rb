40a2d7f522cd67e7a0cdb057dd2ebc7373ed514f
Lauren McCoy <lmccoy8911@gmail.com>
2019-06-08 12:27:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,13 @@ require 'pry'
 class Scraper
 
   @@scraped_students = []
-  @@student_attributes = {}
+
   
   def self.scrape_index_page(index_url)
     html = Nokogiri::HTML(open(index_url))
        
     html.css(".student-card").each do |student| 
+      #binding.pry 
       student_name = student.css(".student-name").text 
       student_name_url = student_name.split(" ").join("-").downcase
       student_location = student.css(".student-location").text
@@ -23,10 +24,17 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    scrape_index_page(profile_url).collect do |student|
-     student_attributes = {:name => student_name}
-      #binding.pry 
-    end 
+    # scrape_index_page(profile_url).collect do |student|
+    #   #binding.pry 
+    # student_attributes = {:name => student[:name] }
+      
+      html = Nokogiri::HTML(open(profile_url))
+      new_html = {}
+        #scrape_index_page(profile_url).each do |student| 
+          
+      
+      
+    
      
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,7 +6,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+   # binding.pry 
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
@@ -17,7 +21,7 @@ class Student
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
