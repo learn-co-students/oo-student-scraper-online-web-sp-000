9f3238e565df12d5b078cdc5db18b8227ac635cc
Alan Krajina <alankrajina88@gmail.com>
2019-06-10 15:05:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,28 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    scraped_students = []
+
+    doc.css(".student-card").each do |student|
     
+      scraped_students = [
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      ]
+    end
+  # binding.pry
+    scraped_students
   end
+
+
 
   def self.scrape_profile_page(profile_url)
     
@@ -13,3 +30,24 @@ class Scraper
 
 end
 
+=begin
+
+[1] pry(Scraper)> scraped_students
+=> [{:name=>"Kevin McCormack",
+  :location=>"New York, NY",
+  :profile_url=>"students/kevin-mccormack.html"}]
+
+
+<div class="student-card" id="ryan-johnson-card">
+ ->           <a href="students/ryan-johnson.html">
+              <div class="view-profile-div">
+                <h3 class="view-profile-text">View Profile</h3>
+              </div>
+              <div class="card-text-container">
+ ->                <h4 class="student-name">Ryan Johnson</h4>
+ ->                <p class="student-location">New York, NY</p>
+              </div>
+            </a>
+          </div>
+
+=end
