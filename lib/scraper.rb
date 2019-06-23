38f50ad5e87887b55fe1be824e0215c260efd942
Andrew Silva <andrewsilva9@gmail.com>
2019-06-23 02:04:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,23 @@
+require 'nokogiri'
+
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read('http://178.128.177.30:42797/fixtures/student-site/')
+
+    student_roster = Nokogiri::HTML(html)
+
+    hash = {}
+
+    sudent_roster.css
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
