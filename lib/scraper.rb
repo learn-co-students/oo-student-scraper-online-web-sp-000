896860b1d645bb3d36ab362b64255e564a3faf60
Connor Cowan <ccowan003@comcast.net>
2019-07-19 23:54:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,21 +9,11 @@ class Scraper
   #is a class method that scrapes the student index page ('./fixtures/student-site/index.html')
   #and a returns an array of hashes in which each hash represents one student
   def self.scrape_index_page(index_url)
-    page = Nokogiri::HTML(open(index_url))
+
+    page = Nokogiri::HTML(index_html)
     students = []
-
-    page.css("div.student-card").each do |student|
-      name = student.css(".student-name").text
-      location = student.css(".student-location").text
-      profile_url = student.css("a").attribute("href").value
-      student_info = {:name => name,
-                :location => location,
-                :profile_url => profile_url}
-      students << student_info
-      end
-    students
-  end
-
+    page = css.()
+    
   end
 
   def self.scrape_profile_page(profile_url)
