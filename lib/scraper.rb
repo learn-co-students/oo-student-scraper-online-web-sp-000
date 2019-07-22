c3fbd1ef4448f5fbc499942dc0a078118ccb7bac
Peter Grant <jgrantps@hotmail.com>
2019-07-22 16:16:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,23 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
+    docs = Nokogiri::HTML(open(profile_url))
 
-  end
+    profile = {}
+# needing linkedin, github, and blog
+    profile[:bio] = docs.css("div.description-holder p").text
+    profile[:profile_quote] = docs.css("div.profile-quote").text
+
+    links = docs.css("div.social-icon-container a")
+    links_array = links.map { |e| e["href"] }
+      links_array.each do |i|
+        profile[:linkedin] = i if i.include?("linkedin")
+        profile[:github] = i if i.include?("github")
+        profile[:twitter] = i if i.include?("twitter")
+        profile[:blog] = i if !profile.has_value?(i)
+      end
+      profile
+      # binding.pry
+    end
 
 end
