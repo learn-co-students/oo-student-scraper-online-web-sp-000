b85eb34202b659d8c517e9141cc1ec7523db7248
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-25 19:58:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,9 +18,57 @@ class Scraper
     students
 # binding.pry
   end
-
+  # def self.scrape_profile_page(profile_url)
+  #       page = Nokogiri::HTML(open(profile_url))
+  #       student = {}
+  #
+  #       # student[:profile_quote] = page.css(".profile-quote")
+  #       # student[:bio] = page.css("div.description-holder p")
+  #       container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+  #       container.each do |link|
+  #         if link.include?("twitter")
+  #           student[:twitter] = link
+  #         elsif link.include?("linkedin")
+  #           student[:linkedin] = link
+  #         elsif link.include?("github")
+  #           student[:github] = link
+  #         elsif link.include?(".com")
+  #           student[:blog] = link
+  #         end
+  #       end
+  #       student[:profile_quote] = page.css(".profile-quote").text
+  #       student[:bio] = page.css("div.description-holder p").text
+  #       student
+  #   end
+  #
+  # end
   def self.scrape_profile_page(profile_url)
-
-  end
-
+    student = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    links = doc.css(".social-icon-container a").collect do |icon|
+      icon.attribute("href").value
+    end
+      links.each do |link|
+        if link.include?("twitter")
+           student[:twitter] = link
+        elsif link.include?("linkedin")
+              student[:linkedin] = link
+        elsif link.include?("github")
+              student[:github] = link
+        elsif link.include?(".com")
+               student[:blog] = link
+        end
+      end
+    student[:profile_quote] = doc.css(".profile-quote").text
+    student[:bio] = doc.css("div.description-holder p").text
+    # student[:bio] = page.css("div.description-holder p").text
+   student
+  #  binding.pry
+ end
+    # twitter
+    # likedin url
+    # github url
+    # blog url
+    # profile quote
+    # bio h3.p.text
 end
