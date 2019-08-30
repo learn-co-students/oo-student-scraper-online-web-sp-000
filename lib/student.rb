fa1fbdeba1b12b536c3add26a8523b4f8dce90d4
Alex Velasquez <jtinnesco@gmail.com>
2019-08-30 03:59:22 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+      Student.new(hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
