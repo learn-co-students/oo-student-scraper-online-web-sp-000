439cd6867f4145faacacc84400caf928303b0f1a
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-29 06:15:01 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,27 +21,27 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-
+    doc = Nokogiri::HTML(open(profile_url))
+    links = doc.css("div.social-icon-container").css("a")             #loop through each link to see if value contains currentlinks.
+    returnable_hash = {}
+    links.each do |link|
+      if link.attribute("href").value.include?("twitter")
+        returnable_hash[:twitter] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("linkedin")
+        returnable_hash[:linkedin] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("github")
+        returnable_hash[:github] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("youtube")
+        returnable_hash[:youtube] = link.attribute("href").value
+      else
+        returnable_hash[:blog] = link.attribute("href").value
+      end
+    end
+    returnable_hash[:profile_quote] = doc.css("div.profile-quote").text
+    returnable_hash[:bio] = doc.css("div.description-holder").css("p").text
+    returnable_hash
   end
 
 end
-
-# puts i.css("div[class='card-text-container']").css("div").css("h4").text
-
-# puts i.css("div[class='card-text-container']").css("div").css("p").text
-# puts i.attribute("href").value
-# puts "========================================================="
-
-# doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
-# student_cards = doc.css("div[class='student-card']").css("a")
-# student_list = []
-# student_cards.each do |i|
-#   student_hash = {
-#         :name => i.css("div[class='card-text-container']").css("div").css("h4").text,
-#         :location => i.css("div[class='card-text-container']").css("div").css("p").text, 
-#         :profile_url => i.attribute("href").value
-#       }
-#   student_list << student_hash
-# end
 
   
--- test/test.rb
+++ test/test.rb
@@ -2,31 +2,39 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
+url = "https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html"
 doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html"))
 
 
-link_container = doc.css("div.social-icon-container").css("a")
-details_container = doc.css("div.details-container")
+# details_container = doc.css("div.details-container")
+# social_container = doc.css("div.social-icon-container").attribute("href").value
 
 
 
-profile_quote = doc.css("div.profile-quote").text
-bio = doc.css("div.description-holder").css("p").text
+# profile_quote = doc.css("div.description-holder").css("p").text
+# bio = doc.css("div.description-holder").css("p").text
 
 
 
-def self.scrape_profile_page(profile_url)
+def scrape_profile_page(profile_url)
   doc = Nokogiri::HTML(open(profile_url))
-  
+  links = doc.css("div.social-icon-container").css("a")             #loop through each link to see if value contains currentlinks.
+  returnable_hash = {}
+  links.each do |link|
+    if link.attribute("href").value.include?("twitter")
+      returnable_hash(:twitter) = link.attribute("href").value
+    elsif link.attribute("href").value.include?("linkedin")
+      returnable_hash(:linkedin) = link.attribute("href").value
+    elsif link.attribute("href").value.include?("github")
+      returnable_hash(:github) = link.attribute("href").value
+    elsif link.attribute("href").value.include?("youtube")
+      returnable_hash(:youtube) = link.attribute("href").value
+    else
+      returnable_hash(:blog) = link.attribute("href").value
+    end
+  returnable_hash(:profile_quote) = doc.css("div.profile-quote").text
+  returnable_hash(:bio) = doc.css("div.description-holder").css("p").text
+  returnable_hash
 end
 
-#this is the end product we are looking for
-  # {
-  #   :twitter=>"http://twitter.com/flatironschool",
-  #   :linkedin=>"https://www.linkedin.com/in/flatironschool",
-  #   :github=>"https://github.com/learn-co",
-  #   :blog=>"http://flatironschool.com",
-  #   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
-  #   :bio=> "I'm a school"
-  # }
-##########
+scrape_profile_page(url)
