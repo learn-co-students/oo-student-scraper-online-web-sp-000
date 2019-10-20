3c515e0c6b43f38a928f9feb7a4b4f9c382ec1f1
Blythe Pollard <blytheepollard@gmail.com>
2019-10-20 03:54:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,18 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #scrapes index that lists all students
+    #uses nokogiri and open uri to access page
+    #return value should be array of hashes 
+    #each hash represents a signle student
+    #keys are :name :location :profile_url
+    #takes a lot of trial and error to find correct selectors
   end
 
   def self.scrape_profile_page(profile_url)
+    #scraping individual student profile page to get further info
+    #return value is hash of key/value pairs describing a student
+    #scrapes twitter, linkedin, github, blog, profile quote, bio--also allows for when these aren't provided
     
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
 class Student
-
+  #uses info returned by scraper.rb methods to create students and add attributes to individual students
+  #student class and scraper class don't direclty interact--shouldn't call on Scraper class in any methods--keeps program flexible
+  #simply is ready to take in information, regardless of its source
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
 
   def initialize(student_hash)
-    
+   
+    #use meta programming to assign new student attributes and values per key/value pairs of hash. Use #send method to achieve this
+    #add newly created student to @@all
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #takes in array of hashes 
+    #iterates over array of hashes to create new student using each hash
   end
 
   def add_student_attributes(attributes_hash)
-    
+    #take hash whose key/value pairs describe additional attributes of a student
+    #iterate over hash and use metaprogramming to dynamically assign student attributes and values using #send method
+    #return value should be student, or self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
