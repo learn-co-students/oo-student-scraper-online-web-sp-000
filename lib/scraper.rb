9d37b902658bbd1c0659fc72b59d17bbde0b1050
Andrew Williams <1williamsaj@gmail.com>
2019-10-22 00:01:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,39 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    student_array = []
+
+    doc.css(".student-card").each do |info|
+      student_hash = {
+        name: info.css(".student-name").text,
+        location: info.css(".student-location").text,
+        profile_url: info.css("a").attribute("href").value
+      }
+      student_array << student_hash
+    end
+    student_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    hash = {}
+  #  binding.pry
+    doc.css("div.main-wrapper.profile .social-icon-container a").each do |info|
+      if info.attribute("href").value.include?("twitter")
+        hash[:twitter] = info.attribute("href").value
+      elsif info.attribute("href").value.include?("linkedin")
+        hash[:linkedin] = info.attribute("href").value
+      elsif info.attribute("href").value.include?("github")
+        hash[:github] = info.attribute("href").value
+      else
+        hash[:blog] = info.attribute("href").value
+      end
+    end
+    hash[:profile_quote] = doc.css("div.profile-quote").text
+    hash[:bio] = doc.css("div.main-wrapper.profile .description-holder p").text
+
+    hash
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,29 @@
+require 'pry'
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(student_hash) # We pass in the array as soon as we make a new student.
+    @name = student_hash[:name] # We extract the value from our name key in the hash.
+    @location = student_hash[:location] # We extract the value from our location key in the hash.
+
+    @@all << self # We save each instance of the student in our '@@all array'
   end
 
   def self.create_from_collection(students_array)
-    
+    #binding.pry
+    students_array.each do |info| # This method iterates over our students array, selecting out each hash.
+        Student.new(info) # We then take this hash and plug it into our initialize method, creating a new student for each instance.
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key,value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all
   end
 end
-
