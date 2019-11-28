d0a8175706acf06e83a4a31a443062bc89b65c06
Michael Carroll <carrollm2@gmail.com>
2019-11-28 03:52:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,27 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    roster_cards = doc.css(".roster-cards-container")
+
+    binding.pry
+    puts roster_cards[0]
+
+    # roster_cards.each do |cards|
+    #   cards.each do |card|
+    #     puts card
+    #     binding.pry
+    #   end
+    # end
+
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
