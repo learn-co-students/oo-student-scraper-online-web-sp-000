d622bcead8e0e2aaa93ba8ca5df8a0b3ebaf5e53
Michael Carroll <carrollm2@gmail.com>
2019-11-30 04:01:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,28 +7,21 @@ class Scraper
     html = open(index_url)
     doc = Nokogiri::HTML(html)
 
-    info = []
-
-
     student_info = []
 
-    # students = doc.css("div.student-card a div.card-text-container")
     students = doc.css("div.student-card")
 
-    students.each do |student|
+    students.each_with_index do |student, index|
 
       name = student.css("div.card-text-container h4.student-name").children.text.strip
       location = student.css("div.card-text-container p.student-location").children.text.strip
-
-      profile = doc.css("div.student-card a")[0]["href"].strip
+      profile = doc.css("div.student-card a")[index]["href"].strip
 
       student_bio = {:name => name, :location => location, :profile_url => profile}
-      # student_bio[:name] => name
-      # student_bio[:location] => location
       student_info << student_bio
     end
     student_info
-    binding.pry
+
 
   end
 
