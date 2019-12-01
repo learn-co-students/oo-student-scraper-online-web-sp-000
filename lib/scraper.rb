48475472f2fa09c84d272ccbc991c39f47462443
Michael Carroll <carrollm2@gmail.com>
2019-12-01 02:42:43 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,6 +27,39 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
 
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    social_media = doc.css("div.social-icon-container a")
+
+    student_info = {}
+
+    social_media.each_with_index do |site, index|
+      site_address = doc.css("div.social-icon-container a")[index]["href"]
+      if site_address.include?("twitter.com")
+        twitter = site_address
+        student_info[:twitter] = twitter
+      elsif site_address.include?("linkedin.com")
+        linkedin = site_address
+        student_info[:linkedin] = linkedin
+      elsif site_address.include?("github.com")
+        github = site_address
+        student_info[:github] = github
+      end
+    end
+
+    binding.pry
+
+    # student_info = {:name => name, :location => location, :profile_url => profile}
+
+    # doc.css("div.social-icon-container a")[0]["href"] =>twitter
+    # doc.css("div.social-icon-container a")[1]["href"] =>linkedin
+    # doc.css("div.social-icon-container a")[2]["href"] =>github
+
+
+
+
+
   end
 
 end
