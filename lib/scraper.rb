85aca7e98d04b437e41806c70f6d693d2d669187
Jason J Cruz <jaycru06@yahoo.com>
2020-01-06 19:35:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    # responsible for scraping the index page that lists 
+    # all of the students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    # responsible for scraping an individual student's profile page 
+    # to get further info about that student 
   end
 
 end
