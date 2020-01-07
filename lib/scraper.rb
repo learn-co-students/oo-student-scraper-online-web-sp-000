bc39d53da249027d327e2441f98a989547bcceda
Jason J Cruz <jaycru06@yahoo.com>
2020-01-07 22:37:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,12 +2,13 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  attr_accessor :name, :location, :profile_url
+  attr_accessor :name, :location, :profile_url, :student_index_array
   
   # get student names: doc.css("div h4.student-name").text.strip
   # get locations: doc.css("p.student-location").text.strip
   # get profile urls: doc.css("div.student-card a").map {|link| link["href"]}
-
+  
+  
   def self.scrape_index_page(index_url)
     # responsible for scraping the index page that lists 
     # all of the students
@@ -17,16 +18,15 @@ class Scraper
     student_names = doc.css("div h4.student-name").text.strip
     student_locations = doc.css("p.student-location").text.strip
     student_profile_urls = doc.css("div.student-card a").map {|link| link["href"]}.join(" ").strip
+    student_names.split.each{|name| self.send((:name), name)}
     # gets each student name stored in array 
-    student_names.split.each do |name| 
-      
-    end 
     # binding.pry 
   end
 
   def self.scrape_profile_page(profile_url)
     # responsible for scraping an individual student's profile page 
     # to get further info about that student 
+    
   end
 
 end
