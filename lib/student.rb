4075125c182abb6b2b3674ecffe2f53b37a82349
Jason J Cruz <jaycru06@yahoo.com>
2020-01-10 05:24:31 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   def self.create_from_collection(students_array)
     # uses the Scraper class to create students with
     # the correct name and location
-    #binding.pry
+    
      
   end
 
