9d69d71e0c2fd16a95d05fc945c1e24829d37932
Jason J Cruz <jaycru06@yahoo.com>
2020-01-10 05:09:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,29 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # takes in an arg of a hash and sets that new student's
+    # attributes using the key/value pairs of that hash.
+    # binding.pry
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    
+    # uses the Scraper class to create students with
+    # the correct name and location
+    #binding.pry
+     
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
