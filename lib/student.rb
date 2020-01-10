a03f65aef86abe9ed80bead10037cf69b45ddfe0
Winston Pemberton <winstonpemberton@gmail.com>
2020-01-10 22:52:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,25 +7,35 @@ class Student
   def initialize(student_hash)
     # becuase you are getting attributes from a hash using the send method makes the most sense
     # iterate though the student_hash argument
-    # use .send(""#{key=}", value")so you can assign each key to :name,:location, etc and gives them the correct value
+    # use .send(""#{key=}", value") on a student object so you can assign each key to :name,:location, etc and gives them the correct value
     # then just put the object into the class variable all
 
     student_hash.each do |key,value|
-      student_info.send("#{key=}, value")
+      self.send("#{key}=", value)
     end
     @@all << self
 
   end
 
   def self.create_from_collection(students_array)
-    # iterate through the argument a hash is passed into it
+    # iterate through the argument, a hash is passed into it
     # create a new student and put the iteration argument inside
     # since each object is initialized with that send method passing a collection assigns all the information that is needed
     # this can now create many objects with the correct information
+
+    students_array.each do |student_info|
+      Student.new(student_info)
+    end
+
+
   end
 
   def add_student_attributes(attributes_hash)
     # same as the initialize method but with a different argument name
+    attributes_hash.each do |key,value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
   def self.all
