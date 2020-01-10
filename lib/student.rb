411eeaafd000d44cb142ed12f84e68ebb44d2e5c
Jason J Cruz <jaycru06@yahoo.com>
2020-01-10 22:15:07 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,14 +9,23 @@ class Student
     # attributes using the key/value pairs of that hash.
     # binding.pry
     student_hash.each {|key, value| self.send(("#{key}="), value)}
-    @@all << self 
+    save 
   end
+  
+  def save 
+    @@all << self 
+  end 
 
   def self.create_from_collection(students_array)
     # uses the Scraper class to create students with
     # the correct name and location
-    
-     
+    # iterate over the array of hashes and create a new individual
+    # student using each hash.
+    #binding.pry
+    students_array.each do |student|
+      Student.new({:name => "#{student[:name]}", :location => "#{student[:location]}"})
+    end
+    #binding.pry
   end
 
   def add_student_attributes(attributes_hash)
