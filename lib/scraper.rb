cd3b0593fdaa79289f1ffa6b2da26453b2720c0b
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-28 00:50:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,38 +4,30 @@ require 'pry'
 
 class Scraper
 
-  # def get_page
-  #   Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-  # end
-
   # responsible for scraping the index page that lists all of the students
   # The return value of this method should be an array of hashes in which
   # each hash represents a single student.
   # The keys of the individual student hashes should be :name, :location and :profile_url
   def self.scrape_index_page(index_url)
-    # binding.pry
-    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-    all_students = doc.css(".student-card")
-      # puts all_students
-    # single_student = get_page.css(".student-card").first
-      # puts single_student
-    first_name = doc.css(".student-card").first.css("h4").text
-    location = doc.css(".student-card").first.css("p").text
-    # i'm close here, but not quite pulling out the url
-    profile_url = doc.css(".student-card").first.css("ahref").text
-    puts first_name
-    puts location
-    puts profile_url
+    doc = Nokogiri::HTML(open(index_url))
+    all_students = []
 
-    # iterate through each
+    doc.css("div.student-card").each do |student_info|
+      name = student_info.css(".student-name").text
+      location = student_info.css(".student-location").text
+      profile_url = student_info.css("a").attribute("href").value
 
-    # student_hash = {
-    #   :name => first_name
-    #   :location =>
-    #   # :profile_url =>
-    # }
-    # puts student_hash
-  end
+        student_hash = {
+          :name => name,
+          :location => location,
+          :profile_url => profile_url
+        }
+
+        all_students << student_hash
+      end
+      all_students
+    end
+
 
   # responsible for scraping an individual student's profile page to get further information about that student.
   # This is a class method that should take in an argument of a student's profile URL.
@@ -43,8 +35,30 @@ class Scraper
   # The return value of this method should be a hash in which the key/value pairs describe an individual student.
   # Some students don't have a Twitter or some other social link. Be sure to be able to handle that.
 
-  # def self.scrape_profile_page(profile_url)
-  #
-  # end
+  def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    indi_student = {}
+
+    # iterate through social links and add to hash
+    social = doc.css(".social-icon-container a").collect {|att| att.attribute("href").value}
+      social.each do |social_link|
+        if social_link.include?("twitter")
+          indi_student[:twitter] = social_link
+        elsif social_link.include?("linkedin")
+          indi_student[:linkedin] = social_link
+        elsif social_link.include?("github")
+          indi_student[:github] = social_link
+        elsif social_link.include?(".com")
+          indi_student[:blog] = social_link
+        end
+      end
+
+      # after loop, add name, bio and quote to hash
+      indi_student[:profile_quote] = doc.css(".profile-quote").text
+      indi_student[:bio] = doc.css(".details-container p").text
+
+    # return hash 
+    indi_student
+  end
 
 end
