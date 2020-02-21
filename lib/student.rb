05cb90fe55a53615b0bec397b4688094ad43a801
Gearz <shanegeary@yahoo.com>
2020-02-21 15:09:41 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,15 +12,19 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+      student = Student.new(hash)
+    end 
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end 
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
