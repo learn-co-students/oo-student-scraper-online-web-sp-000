da75231da0c584bb02f708d1e5e76207dd34646d
github username <github email address>
2020-03-02 12:39:02 -0700

    found values for name location and url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,48 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+
+    doc.css(".student-card")
+
+    binding.pry
   end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+  self.scrape_index_page
+
+  # profile_url = doc.css(".student-card").css("a").attribute("href").value
+  # location = doc.css(".student-card").css(".student-location").text
+  # name = doc.css(".student-card").css(".student-name").text
+
+
+  # def create_project_hash
+  #   html = File.read('fixtures/kickstarter.html')
+  #   kickstarter = Nokogiri::HTML(html)
+
+  #   projects = {}
+
+  #   kickstarter.css("li.project.grid_4").each do |project|
+  #     title = project.css("h2.bbcard_name strong a").text
+  #     projects[title.to_sym] = {
+  #       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+  #       :description => project.css("p.bbcard_blurb").text,
+  #       :location => project.css("ul.project-meta span.location-name").text,
+  #       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  #     }
+  #    end
+
+  #    projects
+
+  # end
+
+
+  # def self.scrape_profile_page(profile_url)
+
+  # end
+
 
 end
-
