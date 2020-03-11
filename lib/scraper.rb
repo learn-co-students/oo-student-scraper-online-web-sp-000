cfae05fe18c3faac1cf4c1c0ed3121489bcbec26
Chance Smith <chancesmith97@hotmail.com>
2020-03-11 19:40:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,45 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
+attr_accessor :students
 
   def self.scrape_index_page(index_url)
-    
+   page = Nokogiri::HTML(open(index_url))
+    students = []
+
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name,
+                :location => location,
+                :profile_url => profile_url}
+      students << student_info
+      end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_html = open(profile_url)
+    profile_doc = Nokogiri::HTML(profile_html)
+    attributes = {}
+    profile_doc.css("div.social-icon-container a").each do |link_xml|
+      case link_xml.attribute("href").value
+      when /twitter/
+        attributes[:twitter] = link_xml.attribute("href").value
+      when /github/
+        attributes[:github] = link_xml.attribute("href").value
+      when /linkedin/
+        attributes[:linkedin] = link_xml.attribute("href").value
+      else
+          attributes[:blog] = link_xml.attribute("href").value
+      end
+    end
+    attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
+    attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
+    attributes
   end
 
 end
