114bbf4a5877cb232fe0b38eaf122a4b3f1d70b0
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2020-03-16 22:58:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = Nokogiri::HTML(open(index_url))
+    students_array = []
+    students.css(".roster-cards-containter .student-card").map do |student|
+    binding.pry
+    student_hash = {}
+    student_name = student.css("a .card-text-container .student-name, h4")
+  end
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
