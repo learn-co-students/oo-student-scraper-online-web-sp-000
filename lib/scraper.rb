3f02e2839e9ef3cfe24b8cda1e2275aef000bc07
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-03-19 18:31:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,16 +21,16 @@ class Scraper
     student = {}
 
     doc = Nokogiri::HTML(open(profile_url))
-    social = doc.css("div.social-icon-container a").collect {|icon| icon}
+    social = doc.css("div.social-icon-container a").collect {|icon| icon.attribute("href")}
     social .each do
       binding.pry
-      if icon.include?(twitter)
+      if social.include?(twitter)
         student[:twitter] = doc.css("div.social-icon-container > a:nth-child(1)").attribute("href").value
-      elsif icon.include?(linkedin)
+      elsif social.include?(linkedin)
         student[:linkedin] =doc.css("div.social-icon-container > a:nth-child(2)").attribute("href").value
-      elsif icon.include?(github)
+      elsif social.include?(github)
         student[:github] =doc.css("div.social-icon-container > a:nth-child(3)").attribute("href").value
-      elsif icon.include?(blog)
+      elsif social.include?(blog)
         student[:blog] =doc.css("div.social-icon-container > a:nth-child(4)").attribute("href").value
       end
     end
