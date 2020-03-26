3b4932769b5ea748a9b5195f9dce471caaf58958
cantstopcoding <jonathan_koss@yahoo.com>
2020-03-26 19:37:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,20 +6,26 @@ class Student
 
   def initialize(student_hash)
     @name = student_hash[:name]
+    @location = student_hash[:location]
     @@all << self
     # binding.pry
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |atr, value|
+      self.send("#{atr}=", value)
+    end 
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
