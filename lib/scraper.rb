531a57424e325b4b349da46d011bb257d601293c
C. Leow <chienleow1@gmail.com>
2020-04-22 19:08:01 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,16 +7,17 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     student_info = []
-    doc.css("div.roster-cards-container").each do |student_card|
-      student_card.css(".student-card a").each do |info|
-        student_info = {
+    # doc.css("div.roster-cards-container").each do |student_card|
+      doc.css(".student-card a").each do |info|
+        student_info_hash = {
         :name => info.css("h4.student-name").text,
         :location => info.css("p.student-location").text,
         :profile_url => info.attribute("href").text
       }
+      student_info << student_info_hash
       end
-    end
-
+    # end
+    student_info
   end
 
   def self.scrape_profile_page(profile_url)
