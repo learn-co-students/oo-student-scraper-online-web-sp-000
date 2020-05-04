8e25ffbc33065799d262fb7a138ed1379cf2a024
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-04 02:27:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,20 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    social_profile = doc.css(".vitals-container a")
+    profile_quote = doc.css(".vitals-container")
+    bio = doc.css(".details-container")
+    social_networks = ["twitter", "linkedin", "github", "blog"]
+    i = 0
+    hash = {}
+    social_profile.each do |e|
+      hash[social_networks[i].to_sym] = e["href"]
+      i += 1
+    end
+    hash[:profile_quote] = profile_quote.css(".profile-quote").text
+    hash[:bio] = bio.css(".description-holder p").text
+    hash
   end
 
 end
