16a673ed830bbfa909797380797a73616de62dc6
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-04 18:20:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,8 +17,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |e|
-      self.all << self.new(e)
+    attributes_hash.each do |k, v|
+      # binding.pry
+      self.send("#{k}=", attributes_hash[k])
+    end
   end
 
   def self.all
