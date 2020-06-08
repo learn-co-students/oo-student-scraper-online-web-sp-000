ccdbe2fe7636307be98941c351d995cc24e5a155
Amy Fong <amyhfong@gmail.com>
2020-06-08 04:16:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 
 class Scraper
-  attr_accessor :name, :location, :profile_url
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
@@ -14,21 +13,34 @@ class Scraper
       student_hash= {}
       student_hash[:name] = student.css(".student-name").text.strip
       student_hash[:location] = student.css(".student-location").text.strip
-      student_hash[:profile_url] = student.css("a[href]").first['href']
-
-# array_of_arrays.each
-# { |record| array_of_hashes << {'name' => record[0], 'number' => record[1].to_i, 'email' => record[2]} }
-
-
+      student_hash[:profile_url] = student.css("a").first['href']
       students << student_hash
-      # binding.pry
-
     end
     students
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student_profile = {}
+    social_links = doc.css(".social-icon-container a")
+    
+    counter = 0
+    while student_profile.length < social_links.length do
+      if social_links[counter]["href"].include?("twitter")
+        student_profile[:twitter] = social_links[counter]["href"]
+      elsif social_links[counter]["href"].include?("linkedin")
+        student_profile[:linkedin] = social_links[counter]["href"]
+      elsif social_links[counter]["href"].include?("github")
+        student_profile[:github] = social_links[counter]["href"]
+      else
+        student_profile[:blog] = social_links[counter]["href"]
+      end
+      counter += 1
+    end
 
+    student_profile[:profile_quote] = doc.css(".profile-quote").text.strip
+    student_profile[:bio] = doc.css(".bio-content.content-holder p").first(p).text
+    student_profile
   end
 
 end
