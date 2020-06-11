4fbfe54d7fb58371979098ffd860e4469ec21d17
elsa chen <snowing222@gmail.com>
2020-06-11 19:47:09 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,51 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+    html=open(index_url)
+    doc=Nokogiri::HTML(html)
+    students=doc.css(".student-card")
+
+    a=students.map do |student|
+      hash={}
+      hash[:name]=student.css(".student-name").text
+      hash[:location]=student.css(".student-location").text
+      hash[:profile_url]=student.css("a").attribute("href").value
+      hash
+    end
+ end
+
+#  {
+#   :twitter=>"http://twitter.com/flatironschool",
+#   :linkedin=>"https://www.linkedin.com/in/flatironschool",
+#   :github=>"https://github.com/learn-co",
+#   :blog=>"http://flatironschool.com",
+#   :profile_quote=>doc.css(".profile-quote").text=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+#   :bio=> doc.css(".description-holder p").text=>"I'm a school"
+# }
+#doc.css(".social-icon-container a")=>return a list of node
+#doc.css(".social-icon-container a")[0].attribute("href").value=>link
+
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    html=open(profile_url)
+    doc=Nokogiri::HTML(html)
+    #Tap
+    #( )?
 
-end
+    student_profile={}
 
+    doc.css(".social-icon-container a").each do |social_media_node|
+      value=social_media_node.attribute("href").value
+      if (value.scan /https:\/\/(www\.)?(\w+)\.*/)==[]
+        key="blog"
+      else
+        key=(value.scan /https:\/\/(www\.)?(\w+)\.*/)[0][-1]
+      end
+      student_profile[key.to_sym]=value
+    end
+    student_profile[:bio]=doc.css(".description-holder p").text
+    student_profile[:profile_quote]=doc.css(".profile-quote").text
+    student_profile
+
+   end
+ end
