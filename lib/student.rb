3886dbe4efac29ffd891c07fd8e4241d5475d3a0
elsa chen <snowing222@gmail.com>
2020-06-11 21:25:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url,
+   :youtube, :facebook, :learn, :instagram
 
   @@all = []
 
