cc7c014726443d9b87b68c9df3e76c2d89e7a566
elsa chen <snowing222@gmail.com>
2020-06-11 21:11:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,13 @@ class Scraper
       hash
     end
  end
+ # # => [
+ #        {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "students/abby-smith.html"},
+ #        {:name => "Joe Jones", :location => "Paris, France", :profile_url => "students/joe-jonas.html"},
+ #        {:name => "Carlos Rodriguez", :location => "New York, NY", :profile_url => "students/carlos-rodriguez.html"},
+ #        {:name => "Lorenzo Oro", :location => "Los Angeles, CA", :profile_url => "students/lorenzo-oro.html"},
+ #        {:name => "Marisa Royer", :location => "Tampa, FL", :profile_url => "students/marisa-royer.html"}
+ #      ]
 
 #  {
 #   :twitter=>"http://twitter.com/flatironschool",
