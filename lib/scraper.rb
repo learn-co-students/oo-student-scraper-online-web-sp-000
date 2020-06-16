b465ae499c73cb890725085a181ffe7d636d906a
Catherine Price <catprice444@gmail.com>
2020-06-16 18:43:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,44 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #scraping the index page that lists all the students
+
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+    students = []
+
+      doc.css(".student-card").each do |student|
+        students_info = {}
+        students_info[:name] = student.css("h4.student-name").text
+        students_info[:location] = student.css("p.student-location").text
+        students_info[:profile_url] = student.css("a").attribute("href").value
+        students << students_info
+      end
+    students
   end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
 
-end
 
+
+    def self.scrape_profile_page(profile_url)
+        student_profile = {}
+        html = open(profile_url)
+        profile = Nokogiri::HTML(html)
+
+        profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+          if social.attribute("href").value.include?("twitter")
+            student_profile[:twitter] = social.attribute("href").value
+          elsif social.attribute("href").value.include?("linkedin")
+            student_profile[:linkedin] = social.attribute("href").value
+          elsif social.attribute("href").value.include?("github")
+            student_profile[:github] = social.attribute("href").value
+          else
+            student_profile[:blog] = social.attribute("href").value
+          end
+        end
+
+        student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+        student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+
+        student_profile
+      end
+    end
