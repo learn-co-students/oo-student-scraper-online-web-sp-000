debe0f34ad1487d72c9b27b0402dd0bd44aceeab
Seth Near <seth.j.near@gmail.com>
2020-07-21 21:24:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,16 @@ require 'pry'
 
 class Scraper
 
-#students: doc.css(".student-card")
-#profile_url: student.css("a").attribute("href").value
-#location: student.css("p").text
-
   def self.scrape_index_page(index_url)
+
     doc = Nokogiri::HTML(open(index_url))
 
     students = []
     doc.css(".student-card").each{ |student_card|
-      student = Hash[ "name" => student_card.css("h4").text,
-                      "location" => student_card.css("p").text,
-                      "profile_url" => student_card.css("a").attribute("href").value
+
+      student = Hash[ :name => student_card.css(".student-name").text,
+                      :location => student_card.css(".student-location").text,
+                      :profile_url => student_card.css("a").attribute("href").value
                     ]
                     students << student
                     }
@@ -24,34 +22,29 @@ class Scraper
 
   end
 
-# twitter: doc.css(".social-icon-container a").first.attribute("href").value
-# linkedin:
 
   def self.scrape_profile_page(profile_url)
-doc = Nokogiri::HTML(open(profile_url))
-profile = {}
-doc.css(".social-icon-container a").each{|link|
-    if link.attribute("href").value.include?("twitter")
-      profile[:twitter] = doc.css(".social-icon-container a").first.attribute("href").value
-    end
+    doc = Nokogiri::HTML(open(profile_url))
 
-    if link.attribute("href").value.include?("linkedin")
-      profile[:linkedin] = doc.css(".social-icon-container a").first.attribute("href").value
-    end
+    profile = {}
 
-    if link.attribute("href").value.include?("github")
-      profile[:github] = doc.css(".social-icon-container a").first.attribute("href").value
-    end
+    doc.css(".social-icon-container a").each{|a|
 
-    if link.attribute("href").value.include?("blog")
-      profile[:blog] = doc.css(".social-icon-container a").first.attribute("href").value
+    if a.css("img").attribute("src").value.include?("twitter")
+      profile["twitter".to_sym] = a.attribute("href").value
+    elsif a.css("img").attribute("src").value.include?("linkedin")
+      profile["linkedin".to_sym] = a.attribute("href").value
+    elsif a.css("img").attribute("src").value.include?("github")
+      profile["github".to_sym] = a.attribute("href").value
+    elsif a.css("img").attribute("src").value.include?("rss")
+      profile["blog".to_sym] = a.attribute("href").value
     end
     }
 
     profile[:profile_quote] = doc.css(".profile-quote").text
 
-    profile[:bio] = doc.css(".description-holder p").
-    return profile
-  end
+    profile[:bio] = doc.css(".description-holder p").text
 
+        return profile
+  end
 end
