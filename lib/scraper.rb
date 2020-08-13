9db50fd5a215a095c5445216b6b7a59b0e0bb17d
Javone <javosmith12@gmail.com>
2020-08-13 14:02:41 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,9 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  # KEYS FOR HASHES ARE :NAME, :LOCATION, :PROFILE_URL
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +12,6 @@ class Scraper
   end
 
 end
+
+scrape_profile_page('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html')
 
