e38b7c7a37cdd6471358918f5650ca1f75169a5d
Chandra Lama <iamme24cl@gmail.com>
2020-10-15 00:34:07 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,18 +2,20 @@ require 'open-uri'
 require 'pry'
 
 =begin
-1. student cards: page.css("div.roster-cards-container").each do |card|
-2. profile_url: card.css(".student-card a").each do |student|
-  "#{student.attr('href')}"
+Student Array:
+1. student cards: page.css("div.roster-cards-container")
+2. profile_url: card.css(".student-card a")
+    "#{student.attr('href')}"
 3. student name: student.css('.student-name').text
 4. student location: student.css('.student-location').text
 =end
 
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-    page = Nokogiri::HTML(open(index_url))
     students = []
+    page = Nokogiri::HTML(open(index_url))
 
     page.css('div.roster-cards-container').each do |card|
       card.css('.student-card a').each do |student|
@@ -29,15 +31,52 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+
+    links = profile_page.css(".social-icon-container").children.css("a").map {|link| link.attribute('href').value}
+
+    links.each do |link|
+      
+      if link.include?('twitter')
+        student[:twitter] = link
+      elsif link.include?('linkedin')
+        student[:linkedin] = link
+      elsif link.include?('github')
+        student[:github] = link
+      elsif
+        student[:blog] = link
+      end
+    end
+
+    student[:profile_quote] = profile_page.css('.profile-quote').text if profile_page.css('.profile-quote').text
+    student[:bio] = profile_page.css('div.bio-content.content-holder div.description-holder p').text if profile_page.css('div.bio-content.content-holder div.description-holder p').text
+
+    student
+    
+
     
   end
 
 end
+
+=begin
+links => profile_page.css(".social-icon-container").children.css("a").map {|link| link.attribute('href').value}
+:twitter => profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value => check if link includes twitter
+:linkedin => profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value => check if link includes linkedin
+:github => profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value => check if link includes github
+:blog => profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value => check if link includes blog
+:profile_quote => profile_page.css('.profile-quote').text => check for profile quote
+:bio => profile_page.css('div.bio-content.content-holder div.description-holder p').text
+=end
+
 
 
 
 
 =begin
 s = Scraper.new
-page = s.class.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/")
+page = s.class.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+
+profile_page = s.class.scrape_profile_page("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html")
 =end
