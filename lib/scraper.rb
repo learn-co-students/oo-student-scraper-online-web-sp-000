606b3c393a62f2383ae9b42e96dcccfad21eb47b
Christerle Nounoute <christerlesn@gmail.com>
2020-11-09 20:12:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     url = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-    index_url = url.css(".student-card")
+    index_url = url.css(".student-card a")
     index_url.map do |index|
-    index_hash = {:name => index.css('h4').text.strip, :location => index.css('p').text.strip, :profile_url => index.css('a').attribute('href').value}
+    index_hash = {:name => index.css('h4').text.strip, :location => index.css('p').text.strip, :profile_url => index['href']}
     index_hash
     end
    #binding.pry
@@ -18,7 +18,16 @@ class Scraper
     #<a href="students/eric-chu.html">
     #index_url.css('a')
   def self.scrape_profile_page(profile_url)
-
+    profile = Nokogiri::HTML(open(profile_url))
+    #binding.pry
+      profile_hash = { twitter: profile.css('.social-icon-container').css('a')[0]['href'],
+        linkedin: profile.css('.social-icon-container').css('a')[1]['href'],
+        github: profile.css('.social-icon-container').css('a')[2]['href'],
+        blog: profile.css('.social-icon-container').css('a')[3]['href'],
+        profile_quote: profile.css('.profile-quote').text,
+        bio: profile.css('.description-holder').css('p').text
+      }
+      profile_hash
   end
 
   #twitter
