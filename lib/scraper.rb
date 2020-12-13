bdb220fae2d0cabc7a77ba73ac7b1ce424a55d33
nllew <n.lewis.perc@gmail.com>
2020-12-12 22:43:56 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,26 +4,37 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    Array.new
-     doc = Nokogiri::HTML(open(index_url))
-     index_array = Array.new
-     index_collection = Hash.new
-
-     doc.css(".student-card").each do |card|
-       index_collection[:name] = card.css("h4.student-name").text
-       index_collection[:location] = card.css("p.student-location").text
-       index_collection[:profile_url] = card.xpath('//div/a/@href')[0].text
-        binding.pry
-       index_array << index_collection
-     end
+    doc = Nokogiri::HTML(open(index_url))
+    index_array = Array.new
+    
+    doc.css(".student-card").each_with_index do |card, index|
+      new_hash = {  
+        :name => card.css("h4.student-name").text,
+        :location => card.css("p.student-location").text,
+        :profile_url => card.xpath('//div/a/@href')[index].text
+      }
+      index_array << new_hash
+    end
+    index_array
   end
-
-# Name
-# Location
-# Profile URL
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    doc = Nokogiri::HTML(open(profile_url))
+    profile_hash = Hash.new
 
+    doc.css("div.social-icon-container a @href").each do |icon| 
+      case
+      when icon.value.include?("twitter")
+        profile_hash.merge!(:twitter => "#{icon}")
+      when icon.value.include?("linkedin")
+        profile_hash.merge!(:linkedin => "#{icon}")
+      when icon.value.include?("github")
+        profile_hash.merge!(:github => "#{icon}")
+      else
+        profile_hash.merge!(:blog => "#{icon}")
+      end
+    end
+    profile_hash.merge!(:profile_quote => doc.css("div.vitals-text-container div.profile-quote").text)
+    profile_hash.merge!(:bio => doc.css("div.description-holder p").text)
+  end
 end
