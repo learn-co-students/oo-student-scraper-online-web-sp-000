40ec9ae05f52180669a6ad6e616a574d1909f914
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-07 23:25:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,16 +18,29 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    doc.css(".vitals-container").each do |data|
-      # the social ones are mixed (i know...shoot me now)
-      twitter = data.css(".social-icon-container").css('a').attribute('href').value
-      linkedin = data.ss(".social-icon-container").css('a')..attribute('href').value
-      github = data.
-      # blog = data. ???
-      profile_quote = data.css(".vitals-text-container").css(".profile-quote").text
-      bio = data.css(."details-container").css(."bio-block details-block").css(".bio-content content-holder").css(".title-holder").css(".description-holder").css('p').text
-      s = {:twitter=>twitter, :linkedin=>linkedin, :github=>github, :blog=>blog, :profile_quote=>profile_quote, :bio=>bio}
-  end
-  s
+    student_hash ={}
+      # the social ones are mixed - grab all URLs
+      social_urls = doc.search(".vitals-container").css(".social-icon-container").search('a').map{ |tag|
+        case tag.name.downcase
+        when 'a'
+            tag['href']
+        end
+      }
+      
+      while social_urls.length > 0
+          find_url = social_urls.shift
+          if find_url.include?("twitter")
+            student_hash[:twitter] = find_url
+          elsif find_url.include?("linkedin")
+            student_hash[:linkedin] = find_url
+          elsif find_url.include?("github")
+            student_hash[:github] = find_url
+          else
+            student_hash[:blog] = find_url
+          end
+        end
+      student_hash[:profile_quote] = doc.search(".vitals-container").css(".vitals-text-container").css(".profile-quote").text
+      student_hash[:bio] = doc.css(".description-holder").css('p').text
+    student_hash
+   end
 end
-
