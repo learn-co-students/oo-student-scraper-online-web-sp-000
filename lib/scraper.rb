3f646aa5516990d860b2efa1b576cb3c9f58b1fc
danny galimi <dannygalimi13@aim.com>
2021-02-18 21:09:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,20 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    new_array = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css(".roster-cards-container").each do |student|
+      student.css(".student-card a").each do |value|
+        profile_url = value.attr("href")
+        name = value.css(".student-name").text
+       
+        location = value.css(".student-location").text
+        binding.pry
+        new_hash = { :name => value.css(".student-name").text
+    
+        end
+    end 
     
   end
 
