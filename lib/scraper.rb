90c61385c60b24283317a03dbc49c367fd08ab26
danny galimi <dannygalimi13@aim.com>
2021-02-19 20:47:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,18 +11,38 @@ class Scraper
       student.css(".student-card a").each do |value|
         profile_url = value.attr("href")
         name = value.css(".student-name").text
-       
-        location = value.css(".student-location").text
-        binding.pry
-        new_hash = { :name => value.css(".student-name").text
-    
-        end
+       location = value.css(".student-location").text
+      new_array << { name: name, location: location, profile_url: profile_url}
+    end
     end 
-    
+    new_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    new_hash = {}
+    new_array = []
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    doc.css(".social-icon-container").children.css("a").each do |value|
+      url = value.attribute("href")
+      new_array << url.value  
+    end 
+    new_array.each do |value|
+      if value.include?("twitter")
+       new_hash[:twitter] = value
+       elsif value.include?("linkedin")
+        new_hash[:linkedin] = value 
+       elsif value.include?("github")
+        new_hash[:github] = value
+      elsif value.include?("joemburgess")
+        new_hash[:blog] = value
+        end 
+    quote = doc.css(".profile-quote").text
+       new_hash[:profile_quote] = quote
+       bio = doc.css(".bio-content.content-holder p").text
+       new_hash[:bio] = bio
+  end
+   new_hash
   end
 
 end
