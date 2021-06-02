1ea28006f8f412616da56c851c35029ca12f6143
Christopher Black <christopher.black.2209@googlemail.com>
2021-06-02 23:08:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    name = doc.css("h4.student_name")
+    binding.pry
+    #name location profile_url
+    # <div class="student-card" id="ryan-johnson-card">
+    #         <a href="students/ryan-johnson.html">
+    #           <div class="view-profile-div">
+    #             <h3 class="view-profile-text">View Profile</h3>
+    #           </div>
+    #           <div class="card-text-container">
+    #             <h4 class="student-name">Ryan Johnson</h4>
+    #             <p class="student-location">New York, NY</p>
+    #           </div>
+    #         </a>
+    #       </div>
   end
 
   def self.scrape_profile_page(profile_url)
