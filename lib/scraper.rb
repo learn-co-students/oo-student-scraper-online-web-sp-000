c123611314e71f2fa66606a3b9b3d22249aa2010
Christopher Black <christopher.black.2209@googlemail.com>
2021-06-04 00:29:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,27 +1,28 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
-    name = doc.css("h4.student_name")
-    binding.pry
-    #name location profile_url
-    # <div class="student-card" id="ryan-johnson-card">
-    #         <a href="students/ryan-johnson.html">
-    #           <div class="view-profile-div">
-    #             <h3 class="view-profile-text">View Profile</h3>
-    #           </div>
-    #           <div class="card-text-container">
-    #             <h4 class="student-name">Ryan Johnson</h4>
-    #             <p class="student-location">New York, NY</p>
-    #           </div>
-    #         </a>
-    #       </div>
+    students_array = []
+    index_doc = Nokogiri::HTML(open(index_url))
+
+    index_doc.css("div.student-card").each do |student_card|
+      student_name = student_card.css(".student-name").text 
+      student_location = student_card.css(".student-location").text
+      student_url = student_card.css("a").attribute("href").value
+      
+      students_array << {name: student_name, location: student_location, profile_url: student_url}
+    end 
+    students_array
   end
 
   def self.scrape_profile_page(profile_url)
+    scrapped_profile_page = {}
+    index_doc = Nokogiri::HTML(open(profile_url))
+    index_doc.css("div.social-icon-container").each do |platform|
+      
     
   end
 
